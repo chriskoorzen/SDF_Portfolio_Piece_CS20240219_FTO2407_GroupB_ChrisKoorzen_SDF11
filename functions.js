0fd3d642ae1ee68baa9deb5542a83bd9ccd59158
Chris Koorzen <fckoorzen@gmail.com>
2024-08-08 15:51:35 +0200

    Upgrade toggle function.

--- functions.js
+++ functions.js
@@ -1,11 +1,22 @@
 "use strict";
 
 const toggleButton = document.getElementById("toggleButton");
-const navMenu = document.getElementById("navMenu");
+const bodyElement = document.getElementsByTagName("body")[0];
 
 function toggleHamburgerMenu(event){
-    navMenu.classList.toggle("hidden");
+    
+    // If we click anywhere, and the menu is open, close it.
+    if (event.currentTarget === bodyElement && !navMenu.classList.contains("hidden")){
+        navMenu.classList.add("hidden");
+        event.stopPropagation();    // Handle event only once
+
+    } else if (event.currentTarget === toggleButton){
+        // Otherwise when triggered via toggleButton, toggle.
+
+        navMenu.classList.toggle("hidden");
+        event.stopPropagation();
+    }
 }
 
 toggleButton.addEventListener("click", toggleHamburgerMenu);
-navMenu.addEventListener("click", toggleHamburgerMenu);
+bodyElement.addEventListener("click", toggleHamburgerMenu);
