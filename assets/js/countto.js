b7031816df0f8753e1f81c2911ec6397bb0d3f8b
Chris Koorzen <fckoorzen@gmail.com>
2024-08-09 13:07:15 +0200

    Fix countto.js so that numbers count on reveal, not page load.

--- assets/js/countto.js
+++ assets/js/countto.js
@@ -1,3 +1,5 @@
+let counts = [];    // Array to keep CountUp object instances
+
 // countTo plugin initialization
 if (document.querySelector("[countTo]")) {
   let numbers = document.querySelectorAll("[countTo]");
@@ -9,19 +11,45 @@ if (document.querySelector("[countTo]")) {
 
     if (number.hasAttribute("data-decimal")) {
       const options = {
-          decimalPlaces: 1,
+          decimalPlaces: 1,
         };
-      countUp = new CountUp(ID, 2.8, options);
+      countUp = new CountUp(ID, 2.8, options);  // TODO: hardcoded, why??
     } else {
       countUp = new CountUp(ID, value);
     }
-
-    if (!countUp.error) {
-      countUp.start();
-    } else {
-      console.error(countUp.error);
-      number.innerHTML = value;
-    }
+    
+    // Add to array for later use
+    counts.push(countUp);
+    
   });
 }
 // end count to plugin initialization
+
+
+// Counting function
+function runNumbers(entries){
+
+  // hack to fix Observer API
+  // Because it will always fire on mount
+  if (!entries[0].isIntersecting){
+    return;
+  }
+
+  counts.forEach((countUp) => {
+        if (!countUp.error) {
+          countUp.start();
+        } else {
+          console.error(countUp.error);
+        }
+    });
+}
+
+// Add in intersection observer
+let obsOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1.0
+}
+let observer = new IntersectionObserver(runNumbers, obsOptions);
+let target = document.querySelector("#stats");
+observer.observe(target);
